c144494480c2ce1fcf2e1bf04c9319249bcfdffe
inu0104 <inu0104@gmail.com>
2023-04-29 20:28:04 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import  setup
+
+setup(
+    name='cosi',
+    version='0.0.1',
+    description='FIC coin simulator',
+    url='https://github.com/TIC-Quant/coin_simulator.git',
+    author=['DoyoonKim', 'InwooTae'],
+    author_email=['vitainu0104@unist.ac.kr','dyk6208@gmail.com'],
+    license= 'FIC',
+    packages=['cosi'],
+    zip_safe=False,
+    install_requires=[]
+)
