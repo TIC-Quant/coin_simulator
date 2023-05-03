21bb8504b42659dedb11d4986b20151da0c6381e
cosmos1030 <dyk6208@gmail.com>
2023-05-03 04:53:30 +0000

    days error fix

--- cosi/data_prep.py
+++ cosi/data_prep.py
@@ -5,10 +5,12 @@ import datetime
 
 def get_ohlc(symbol, start_date=None, end_date=None):
     symbol = "KRW-"+symbol
-    start_date = int(start_date.replace('-', ''))
-    end_date = int(end_date.replace('-', ''))
-    count = end_date - start_date
-    df = pu.get_ohlcv(symbol, to = str(end_date), count=count)
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    delta = end_date - start_date
+    start_date = datetime.datetime.strftime(start_date, "%Y%m%d")
+    end_date = datetime.datetime.strftime(end_date, "%Y%m%d")
+    df = pu.get_ohlcv(symbol, to = str(end_date), count=delta.days)
     df.index = [datetime.datetime.strptime(datewithtime.strftime('%Y-%m-%d'), '%Y-%m-%d') for datewithtime in df.index]
     df = df.rename(columns={'open': 'Open', 'high': 'High', 'low':'Low', 'close':'Close', 'volume':'Volume'})
     df['Adj Close'] = df['Close']
