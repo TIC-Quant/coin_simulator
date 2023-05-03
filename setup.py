1a4030a7741e36b869a4996a74539adf96087ef4
cosmos1030 <dyk6208@gmail.com>
2023-05-03 05:01:13 +0000

    days error fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import  setup
 
 setup(
     name='cosi',
-    version='0.0.1',
+    version='0.0.2',
     description='FIC coin simulator',
     url='https://github.com/TIC-Quant/coin_simulator.git',
     author='DoyoonKim',
